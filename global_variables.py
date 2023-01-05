27180729f9883fcf9cecbb493d74675a3086ae27
bvuongle <01184762@pw.edu.pl>
2023-01-05 15:05:58 +0100

    Add global variables and libraries

---
+++ global_variables.py
@@ -0,0 +1,9 @@
+# default value
+N = 5
+maxInLines = 5
+topView = [0, 0, 3, 0, 1]
+botView = [0, 0, 0, 3, 4]
+rightView = [4, 4, 0, 1, 0]
+leftView = [0, 0, 3, 0, 0]
+originalMap = []
+finalMap = []
